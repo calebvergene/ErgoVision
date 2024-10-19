be473a2b7cdfe0c09073a3a96577a03d30d2be57
Arjun Dabir <arjunadabir@gmail.com>
2024-10-19 12:11:05 -0700

    feat: body parts container

---
+++ client/src/components/dashboard/body-part-card.tsx
@@ -0,0 +1,11 @@
+const BodyPartCard = () => {
+  return (
+    <div className="h-full flex-1 rounded-md border">
+      <h1>0°</h1>
+    </div>
+  )
+}
+
+export default BodyPartCard
+
+// neck, upper arm, lower arm, trunk and wrist
---
+++ client/src/components/dashboard/body-part-container.tsx
@@ -0,0 +1,16 @@
+import BodyPartCard from './body-part-card'
+
+const BodyPartContainer = () => {
+  return (
+    <div>
+      <h1 className="text-lg font-semibold">By Body Part</h1>
+      <div className="rounded-m flex h-32 w-full gap-2 rounded-md bg-white">
+        {[0, 0, 0].map((_, index) => (
+          <BodyPartCard key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BodyPartContainer
