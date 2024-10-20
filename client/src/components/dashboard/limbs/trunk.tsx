0769d13913a609b35d6fe651c7f856817ac9f7cf
Arjun Dabir <arjunadabir@gmail.com>
2024-10-20 03:54:25 -0700

    updated trunk

--- client/src/components/dashboard/limbs/trunk.tsx
+++ client/src/components/dashboard/limbs/trunk.tsx
@@ -14,27 +14,52 @@ const trunkImages = {
 
 const trunkImageOrder = ['0°', '0-20°', '20-60°', '60°']
 
-const Trunk = () => (
-  <div className="flex w-full flex-1 flex-col p-2">
-    <h1 className="text-2xl font-semibold text-[#545F71]">Trunk</h1>
-    <div className="flex flex-1 gap-1 rounded-md bg-[#F0EFEF] px-2 py-4">
-      {trunkImageOrder.map((item, index) => (
-        <div key={index} className="flex flex-1 flex-col">
-          <h1 className="w-full rounded-t-md bg-[#979797] text-center font-semibold text-white">
-            {item}
-          </h1>
-          <div className="flex w-full flex-1 justify-center rounded-b-md bg-white">
-            <Image
-              src={trunkImages[item as keyof typeof trunkImages]}
-              alt={item}
-              height={150}
-              className="object-contain"
-            />
+const getBackgroundColor = (degree: number, range: string) => {
+  const [start, end] = trunkRange[range as keyof typeof trunkRange]
+  if (end) {
+    return degree >= start && degree <= end ? 'bg-[#0161E8]' : ''
+  }
+  return degree === start ? 'bg-[#0161E8]' : ''
+}
+
+const trunkRange = {
+  '0°': [0],
+  '0-20°': [0, 20],
+  '20-60°': [20, 60],
+  '60°': [60, 100],
+}
+
+const Trunk = ({ degree }: { degree: number }) => {
+  const activeRange =
+    trunkImageOrder.find((range) => {
+      const [start, end] = trunkRange[range as keyof typeof trunkRange]
+      return end ? degree >= start && degree <= end : degree === start
+    }) || '0°'
+
+  return (
+    <div className="flex w-full flex-1 flex-col p-2">
+      <h1 className="text-2xl font-semibold text-[#545F71]">Trunk</h1>
+      <div className="flex flex-1 gap-1 rounded-md bg-[#F0EFEF] px-2 py-4">
+        {trunkImageOrder.map((item, index) => (
+          <div key={index} className="flex flex-1 flex-col">
+            <h1
+              className={`w-full rounded-t-md text-center font-semibold text-white ${getBackgroundColor(degree, item) || (item === activeRange ? 'bg-blue-400' : 'bg-[#979797]')}`}
+            >
+              {item}
+            </h1>
+            <div className="flex w-full flex-1 justify-center rounded-b-md bg-white">
+              <Image
+                src={trunkImages[item as keyof typeof trunkImages]}
+                alt={item}
+                height={150}
+                className="object-contain"
+              />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Trunk
