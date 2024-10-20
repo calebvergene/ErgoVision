b188ab30c0ad7460732ad435e0546e7e0802e124
Arjun Dabir <arjunadabir@gmail.com>
2024-10-20 03:53:19 -0700

    updated views

--- client/src/components/dashboard/critical-frames-view.tsx
+++ client/src/components/dashboard/critical-frames-view.tsx
@@ -9,19 +9,43 @@ import Wrist from './limbs/wrist'
 const CriticalFramesView = ({
   criticalFramesView,
 }: {
-  criticalFramesView: string
+  criticalFramesView: number // assuming this is a number representing the keyframe
 }) => {
-  const { setCriticalFramesView } = useContext(ContentContext)
+  const { fastapiResponse, setCriticalFramesView } = useContext(ContentContext)
+
+  // Find the keyframe based on the criticalFramesView (e.g., 95 or 103)
+  const frame = fastapiResponse?.critical_frames.find(
+    (frame) =>
+      frame.critical_limbs &&
+      Object.keys(frame.critical_limbs).includes(criticalFramesView.toString()),
+  )
+
   return (
     <div className="flex h-full w-full gap-2 rounded-md bg-[#F9F9F9] p-2">
       <div className="flex h-full w-1/2 flex-col gap-2 rounded-md bg-white">
-        <div className="aspect-video w-full rounded-md bg-black"></div>
+        <div className="flex aspect-video w-full items-center justify-center rounded-md bg-white">
+          {frame ? (
+            <img
+              src={`/${frame.img}`}
+              alt="Keyframe image"
+              className="max-h-80"
+            />
+          ) : (
+            <p>No keyframe found for {criticalFramesView}</p>
+          )}
+        </div>
         <CriticalLimbs />
       </div>
       <div className="flex h-full w-1/2 flex-col gap-2 rounded-md bg-white">
-        <Trunk />
-        <LowerArm />
-        <Wrist />
+        <Trunk
+          degree={frame?.critical_limbs?.[criticalFramesView]?.[0]?.trunk ?? 0}
+        />
+        <Wrist degree={0} />
+        <LowerArm
+          degree={
+            frame?.critical_limbs?.[criticalFramesView]?.[2]?.lower_arm ?? 0
+          }
+        />
       </div>
       <button
         onClick={() => setCriticalFramesView(null)}
