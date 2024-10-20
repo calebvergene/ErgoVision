4ca8dfce2cfa99f615abc4af367e275d6ac64e35
Arjun Dabir <arjunadabir@gmail.com>
2024-10-20 03:50:26 -0700

    fasdf

--- client/src/components/dashboard/video.tsx
+++ client/src/components/dashboard/video.tsx
@@ -1,10 +1,20 @@
+'use client'
+
+import { ContentContext } from '@/components/dashboard/content'
+import { useContext } from 'react'
+
 const VideoWrapper = () => {
+  const { fastapiResponse } = useContext(ContentContext)
+  const videoUrl =
+    fastapiResponse && fastapiResponse.video
+      ? `/${fastapiResponse.video}`
+      : '/dashboard/testvideo.mp4'
   return (
     <video
       controls
+      src={videoUrl}
       className="flex aspect-video h-fit w-full rounded-md bg-white"
     >
-      <source src="/dashboard/testvideo.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   )
